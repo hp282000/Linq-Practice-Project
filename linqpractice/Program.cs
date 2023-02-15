6ca80a8fc6eaef87a5dadd202063f50a0d44a7fa
Harsh <harsp24@gmail.com>
2023-02-15 11:11:54 +0530

    Add project files.

---
+++ linqpractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System;
+using System.Xml.Linq;
+
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+        string[] name1 = { "hp", "sd", "pt", "as" };
+
+        //LinqQuery
+
+        //var linqquery = from n in name1
+        //                where n.Contains('s')
+        //                select n;
+
+        //foreach (var name in linqquery)
+        //{
+        //    Console.WriteLine(name);
+        //}
+
+
+        //Method Syntax
+
+        var result = name1.Where(nam =>  nam.Contains('a'));
+
+        //Console.WriteLine(result.FirstOrDefault[0]);
+        foreach (var name2 in result) {
+            Console.WriteLine(name2);
+        }
+    }
+}
+
